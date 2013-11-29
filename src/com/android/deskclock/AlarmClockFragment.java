26f3b2feb2a2d757301eb29eddb0b1e71ac26c00
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2013-11-29 16:55:52 +0400

    DeskClock: Add increasing volume option for alarm clocks
    Author: Austen Dicken <cvpcsm@gmail.com>
    Change-Id: I432f14d17aa8965a9267227a5ee892b6eab30caf

--- src/com/android/deskclock/AlarmClockFragment.java
+++ src/com/android/deskclock/AlarmClockFragment.java
@@ -532,6 +532,7 @@ public class AlarmClockFragment extends DeskClockFragment implements
             LinearLayout repeatDays;
             Button[] dayButtons = new Button[7];
             CheckBox vibrate;
+            CheckBox increasingVolume;
             TextView ringtone;
             View hairLine;
             View arrow;
@@ -676,6 +677,7 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 holder.dayButtons[i] = dayButton;
             }
             holder.vibrate = (CheckBox) view.findViewById(R.id.vibrate_onoff);
+            holder.increasingVolume = (CheckBox) view.findViewById(R.id.increasing_volume_onoff);
             holder.ringtone = (TextView) view.findViewById(R.id.choose_ringtone);
 
             view.setTag(holder);
@@ -990,6 +992,19 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 @Override
                 public void onClick(View view) {
                     launchRingTonePicker(alarm);
+                }
+            });
+
+
+            itemHolder.increasingVolume.setVisibility(View.VISIBLE);
+            itemHolder.increasingVolume.setChecked(alarm.increasingVolume);
+            itemHolder.increasingVolume.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final boolean checked = ((CheckBox) v).isChecked();
+                    //When action mode is on - simulate long click
+                    alarm.increasingVolume = checked;
+                    asyncUpdateAlarm(alarm, false);
                 }
             });
         }
