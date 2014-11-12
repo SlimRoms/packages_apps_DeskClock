55db6e0dcc9a9a3678f1b713426be576416199f9
Muhammed Nazim <mnm9994u@gmail.com>
2014-11-12 12:35:57 -0800

    Forward Port: Add option to hide AlarmClock Icon in StatusBar [2/2]
    Change-Id: If538c30cfa331203ea82afe9b281faaa1d87e6f6

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -17,6 +17,8 @@
 package com.android.deskclock;
 
 import android.app.ActionBar;
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.media.AudioManager;
@@ -31,6 +33,7 @@ import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.deskclock.alarms.AlarmNotifications;
 import com.android.deskclock.worldclock.Cities;
 
 import java.util.ArrayList;
@@ -49,6 +52,8 @@ public class SettingsActivity extends PreferenceActivity
 
     public static final String KEY_ALARM_IN_SILENT_MODE =
             "alarm_in_silent_mode";
+    public static final String KEY_SHOW_STATUS_BAR_ICON =
+            "show_status_bar_icon";
     public static final String KEY_ALARM_SNOOZE =
             "snooze_duration";
     public static final String KEY_VOLUME_BEHAVIOR =
@@ -75,7 +80,7 @@ public class SettingsActivity extends PreferenceActivity
 
     private static CharSequence[][] mTimezones;
     private long mTime;
-
+    private SwitchPreference mAlarmIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,6 +105,10 @@ public class SettingsActivity extends PreferenceActivity
         listPref.setEntries(mTimezones[1]);
         listPref.setSummary(listPref.getEntry());
         listPref.setOnPreferenceChangeListener(this);
+
+        mAlarmIcon = (SwitchPreference) findPreference(KEY_SHOW_STATUS_BAR_ICON);
+        mAlarmIcon.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.SHOW_ALARM_ICON, 1) == 1);
     }
 
     @Override
@@ -187,6 +196,11 @@ public class SettingsActivity extends PreferenceActivity
         } else if (KEY_SHAKE_ACTION.equals(pref.getKey())) {
             final ListPreference listPref = (ListPreference) pref;
             updateActionSummary(listPref, (String) newValue, R.string.shake_action_summary);
+        } else if (KEY_SHOW_STATUS_BAR_ICON.equals(pref.getKey())) {
+            // Check if any alarms are active. If yes and
+            // we allow showing the alarm icon, the icon will be shown.
+            Settings.System.putInt(getContentResolver(), Settings.System.SHOW_ALARM_ICON,
+                    (Boolean) newValue ? 1 : 0);
         }
         return true;
     }
@@ -247,6 +261,9 @@ public class SettingsActivity extends PreferenceActivity
         listPref = (ListPreference) findPreference(KEY_SHAKE_ACTION);
         updateActionSummary(listPref, listPref.getValue(), R.string.shake_action_summary);
         listPref.setOnPreferenceChangeListener(this);
+
+        SwitchPreference hideStatusbarIcon = (SwitchPreference) findPreference(KEY_SHOW_STATUS_BAR_ICON);
+        hideStatusbarIcon.setOnPreferenceChangeListener(this);
 
         SnoozeLengthDialog snoozePref = (SnoozeLengthDialog) findPreference(KEY_ALARM_SNOOZE);
         snoozePref.setSummary();
