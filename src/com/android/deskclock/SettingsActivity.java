LineageOS/android_packages_apps_DeskClock

DeskClock: Add back flip and shake actions
Cherry-picked and ported from:
 * http://review.cyanogenmod.org/#/c/29894/

Change-Id: I222b6cd53a95db6f8662e7aef52aa6922d8a5d6a
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

src/com/android/deskclock/SettingsActivity.java
@@ -56,6 +56,10 @@ public class SettingsActivity extends PreferenceActivity
             "snooze_duration";
     public static final String KEY_VOLUME_BEHAVIOR =
             "volume_button_setting";
+    public static final String KEY_FLIP_ACTION =
+            "flip_action";
+    public static final String KEY_SHAKE_ACTION =
+            "shake_action";
     public static final String KEY_AUTO_SILENCE =
             "auto_silence";
     public static final String KEY_CLOCK_STYLE =
@@ -176,6 +180,12 @@ public class SettingsActivity extends PreferenceActivity
             final ListPreference listPref = (ListPreference) pref;
             final int idx = listPref.findIndexOfValue((String) newValue);
             listPref.setSummary(listPref.getEntries()[idx]);
+        } else if (KEY_FLIP_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            updateActionSummary(listPref, (String) newValue, R.string.flip_action_summary);
+        } else if (KEY_SHAKE_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            updateActionSummary(listPref, (String) newValue, R.string.shake_action_summary);
         } else if (KEY_SHOW_STATUS_BAR_ICON.equals(pref.getKey())) {
             // Check if any alarms are active. If yes and
             // we allow showing the alarm icon, the icon will be shown.
@@ -205,6 +215,11 @@ public class SettingsActivity extends PreferenceActivity
         sendBroadcast(i);
     }
 
+    private void updateActionSummary(ListPreference listPref, String action, int summaryResId) {
+        int i = Integer.parseInt(action);
+        listPref.setSummary(getString(summaryResId,
+            getResources().getStringArray(R.array.action_summary_entries)[i]));
+    }
 
     private void refresh() {
         ListPreference listPref = (ListPreference) findPreference(KEY_AUTO_SILENCE);
@@ -226,6 +241,14 @@ public class SettingsActivity extends PreferenceActivity
 
         listPref = (ListPreference) findPreference(KEY_VOLUME_BUTTONS);
         listPref.setSummary(listPref.getEntry());
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_FLIP_ACTION);
+        updateActionSummary(listPref, listPref.getValue(), R.string.flip_action_summary);
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_SHAKE_ACTION);
+        updateActionSummary(listPref, listPref.getValue(), R.string.shake_action_summary);
         listPref.setOnPreferenceChangeListener(this);
 
         CheckBoxPreference hideStatusbarIcon = (CheckBoxPreference) findPreference(KEY_SHOW_STATUS_BAR_ICON);

SlimRoms/packages_apps_DeskClock

src/com/android/deskclock/SettingsActivity.java
@@ -46,6 +46,10 @@ public class SettingsActivity extends PreferenceActivity
             "snooze_duration";
     public static final String KEY_VOLUME_BEHAVIOR =
             "volume_button_setting";
+    public static final String KEY_FLIP_ACTION =
+            "flip_action";
+    public static final String KEY_SHAKE_ACTION =
+            "shake_action";
     public static final String KEY_AUTO_SILENCE =
             "auto_silence";
     public static final String KEY_CLOCK_STYLE =
@@ -146,6 +150,12 @@ public class SettingsActivity extends PreferenceActivity
             final ListPreference listPref = (ListPreference) pref;
             final int idx = listPref.findIndexOfValue((String) newValue);
             listPref.setSummary(listPref.getEntries()[idx]);
+        } else if (KEY_FLIP_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            updateActionSummary(listPref, (String) newValue, R.string.flip_action_summary);
+        } else if (KEY_SHAKE_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            updateActionSummary(listPref, (String) newValue, R.string.shake_action_summary);
         }
         return true;
     }
@@ -171,6 +181,11 @@ public class SettingsActivity extends PreferenceActivity
         sendBroadcast(i);
     }
 
+    private void updateActionSummary(ListPreference listPref, String action, int summaryResId) {
+        int i = listPref.findIndexOfValue(action);
+        listPref.setSummary(getString(summaryResId,
+            getResources().getStringArray(R.array.action_summary_entries)[i]));
+    }
 
     private void refresh() {
         ListPreference listPref = (ListPreference) findPreference(KEY_AUTO_SILENCE);
@@ -192,6 +207,14 @@ public class SettingsActivity extends PreferenceActivity
 
         listPref = (ListPreference) findPreference(KEY_VOLUME_BUTTONS);
         listPref.setSummary(listPref.getEntry());
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_FLIP_ACTION);
+        updateActionSummary(listPref, listPref.getValue(), R.string.flip_action_summary);
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_SHAKE_ACTION);
+        updateActionSummary(listPref, listPref.getValue(), R.string.shake_action_summary);
         listPref.setOnPreferenceChangeListener(this);
 
         SnoozeLengthDialog snoozePref = (SnoozeLengthDialog) findPreference(KEY_ALARM_SNOOZE);